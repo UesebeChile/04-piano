3d75a8bc622dbe758c7fed2b681e5ce840129d89
Cristian Torres <ctorres@uesebe.cl>
2019-01-28 13:14:55 -0300

    Initial Commit

---
+++ 04-piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  04-piano
+//
+//  Created by Cristian Torres on 1/28/19.
+//  Copyright © 2019 Cristian Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
